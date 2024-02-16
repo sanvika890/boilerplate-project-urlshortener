8b38d5af460d270744c80dea712e8571873bc629
Sanvika Kumar <sanvika890@gmail.com>
2024-02-16 19:10:09 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
+const dns = require('node:dns')
 const express = require('express');
 const cors = require('cors');
-const app = express();
-
+let bodyParser = require("body-parser");
+let app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port =  3000;
+
 
 app.use(cors());
 
@@ -18,6 +22,21 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post("/api/shorturl",function(req,res){
+  const url = req.body.url
+  dns.lookup(url,(err,data,family)=>{
+    if(err===null){
+      const random = Math.floor(Math.random(0,1)*100)
+      app.get("/api/shorturl/"+random,function(req,res){
+        res.redirect(url)
+      })
+      res.json({original_url : url, short_url : random})
+    }else{
+      res.json({ error: 'invalid url' })
+    }
+  })
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
